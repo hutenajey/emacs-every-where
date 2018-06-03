cf7c88df78092a85aab6924211e8fdc420e00f44
coolor <hutenajey@gmail.com>
2018-06-03 11:14:25 +0800

    update for python

--- init.el
+++ init.el
@@ -191,6 +191,7 @@
 (require 'init-editing-utils)
 (require 'init-fold)
 (require 'init-htmlize)
+(require 'init-py3)
 ;; ;;----------------------------------------------------------------------------
 ;; ;; Allow access from emacsclient
 ;; ;;----------------------------------------------------------------------------
@@ -258,3 +259,10 @@
 (custom-set-variables
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
 
+(add-to-list 'package-archives'
+             ("elpa" . "http://tromey.com/elpa/") t)
+(add-to-list 'package-archives'
+             ("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives'
+             ("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
